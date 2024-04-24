b942ac8c5b3dbfd7f593b511e5cf87452d4305ac
InvalidJokerDE <0limitdev@gmail.com>
2024-04-24 11:26:55 +0200

    feat: translate

--- src/main/kotlin/net/blockventuremc/modules/archievements/AchievementManager.kt
+++ src/main/kotlin/net/blockventuremc/modules/archievements/AchievementManager.kt
@@ -4,22 +4,36 @@ import dev.fruxz.ascend.extension.logging.getItsLogger
 import dev.fruxz.stacked.extension.Title
 import dev.fruxz.stacked.text
 import net.blockventuremc.database.functions.addAchievementToUser
+import net.blockventuremc.database.functions.getAchievementOfUser
 import net.blockventuremc.database.model.DatabaseAchievement
+import net.blockventuremc.extensions.sendMessagePrefixed
+import net.blockventuremc.extensions.toDatabaseUser
+import net.blockventuremc.extensions.translate
 import net.blockventuremc.modules.archievements.model.Achievement
 import org.bukkit.Bukkit
 import java.util.*
 
 object AchievementManager {
-    fun addAchievement(uuid: UUID, achievement: Achievement) {
-        // TODO: Notify User
+    private fun addAchievement(uuid: UUID, achievement: Achievement) {
 
         val player = Bukkit.getPlayer(uuid) ?: return
+        val title = player.toDatabaseUser().translate("achievement.${achievement.name.lowercase()}.title")?.message ?: achievement.title
+        val description = player.toDatabaseUser().translate("achievement.${achievement.name.lowercase()}.description")?.message ?: achievement.description
 
-        player.showTitle(Title(text(achievement.title), text(achievement.description)))
+        player.showTitle(Title(text(title), text(description)))
+
+        player.sendMessagePrefixed(player.toDatabaseUser().translate("achievement.achieved", mapOf("achievement" to title))
+            ?.message ?: "Achievement achieved: $title")
 
         addAchievementToUser(DatabaseAchievement(uuid, achievement))
 
-        getItsLogger().info("Added achievement ${achievement.title} to ${player.name}")
+        getItsLogger().info("Added achievement $title to ${player.name}")
 
+    }
+
+    fun addIfNotAchieved(uuid: UUID, achievement: Achievement) {
+        if (getAchievementOfUser(uuid, achievement) == null) {
+            addAchievement(uuid, achievement)
+        }
     }
 }
--- src/main/kotlin/net/blockventuremc/modules/archievements/events/FirstJoinEvent.kt
+++ src/main/kotlin/net/blockventuremc/modules/archievements/events/FirstJoinEvent.kt
@@ -1,6 +1,5 @@
 package net.blockventuremc.modules.archievements.events
 
-import net.blockventuremc.database.functions.getAchievementOfUser
 import net.blockventuremc.modules.archievements.AchievementManager
 import net.blockventuremc.modules.archievements.model.Achievement
 import org.bukkit.event.EventHandler
@@ -11,10 +10,6 @@ import org.bukkit.event.player.PlayerJoinEvent
 class FirstJoinEvent: Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     fun onFirstJoin(event: PlayerJoinEvent) {
-        val achievement = getAchievementOfUser(event.player.uniqueId, Achievement.FirstJoin)
-
-        if (achievement == null) {
-            AchievementManager.addAchievement(event.player.uniqueId, Achievement.FirstJoin)
-        }
+        AchievementManager.addIfNotAchieved(event.player.uniqueId, Achievement.FirstJoin)
     }
 }
