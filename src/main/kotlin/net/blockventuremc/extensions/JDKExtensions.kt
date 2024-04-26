b03de0015f2d8a27e5eb0d5a1cee06cb414ca4da
InvalidJokerDE <0limitdev@gmail.com>
2024-04-26 22:24:49 +0200

    fix: use our lang class

--- src/main/kotlin/net/blockventuremc/extensions/JDKExtensions.kt
+++ src/main/kotlin/net/blockventuremc/extensions/JDKExtensions.kt
@@ -1,6 +1,8 @@
 package net.blockventuremc.extensions
 
 import dev.kord.common.Locale
+import dev.kord.common.asJavaLocale
+import net.blockventuremc.modules.general.model.Languages
 import org.slf4j.LoggerFactory
 
 fun <T : Any> T.getLogger(): org.slf4j.Logger {
@@ -12,8 +14,5 @@ fun <T : Any> T.nullIf(condition: (T) -> Boolean): T? {
 }
 
 val Locale.code: String
-    get() = when (this) {
-        Locale.GERMAN -> "de-DE"
-        Locale.ENGLISH_UNITED_STATES -> "en-US"
-        else -> "en-US"
-    }
+    get() = Languages.entries.firstOrNull { it.locale == this.asJavaLocale() }?.getLanguageCode()
+        ?: throw IllegalArgumentException("Locale $this is not supported")
