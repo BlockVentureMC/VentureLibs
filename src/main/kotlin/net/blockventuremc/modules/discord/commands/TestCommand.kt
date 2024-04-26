b8924690817e53ff7332bce5b7337b99ca2468c1
InvalidJokerDE <0limitdev@gmail.com>
2024-04-26 22:39:48 +0200

    update: log test cmd

--- src/main/kotlin/net/blockventuremc/modules/discord/commands/TestCommand.kt
+++ src/main/kotlin/net/blockventuremc/modules/discord/commands/TestCommand.kt
@@ -6,6 +6,7 @@ import dev.kord.core.behavior.interaction.respondEphemeral
 import dev.kord.core.entity.interaction.GuildChatInputCommandInteraction
 import dev.kord.rest.builder.interaction.ChatInputCreateBuilder
 import dev.kord.rest.builder.interaction.integer
+import net.blockventuremc.modules.discord.manager.ChannelManager
 import net.blockventuremc.modules.discord.model.AbstractCommand
 import net.blockventuremc.utils.translate
 
@@ -23,6 +24,12 @@ class TestCommand: AbstractCommand() {
     }
 
     override suspend fun execute(bot: Kord, interaction: GuildChatInputCommandInteraction) {
+
+        ChannelManager.sendEconomy {
+            title = "Test"
+            description = "You entered ${interaction.command.options["number"]?.value}"
+        }
+
         interaction.respondEphemeral {
             content = "You entered ${interaction.command.options["number"]?.value}"
         }
