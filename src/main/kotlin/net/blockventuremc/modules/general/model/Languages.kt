f325e30700e8381a5068925b1783c5700af8dd52
Liam Sage <mleaked@gmail.com>
2024-04-27 18:47:29 +0200

    docs: Added French and Dutch (#71)

--- src/main/kotlin/net/blockventuremc/modules/general/model/Languages.kt
+++ src/main/kotlin/net/blockventuremc/modules/general/model/Languages.kt
@@ -4,12 +4,16 @@ import java.util.Locale
 
 enum class Languages(val locale: Locale) {
     DE(Locale.GERMAN),
-    EN(Locale.US);
+    EN(Locale.US),
+    FR(Locale.FRENCH),
+    NL(Locale("nl"));
 
     fun getLanguageCode(): String {
         return when (this) {
             DE -> "de-DE"
             EN -> "en-US"
+            FR -> "fr-FR"
+            NL -> "nl-NL"
         }
     }
 }
