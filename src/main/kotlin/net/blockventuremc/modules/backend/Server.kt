935bce2c11949e442e564dfbb6282dd037f29076
InvalidJokerDE <0limitdev@gmail.com>
2024-04-28 19:35:11 +0200

    feat: audio backend group

--- src/main/kotlin/net/blockventuremc/modules/backend/Server.kt
+++ src/main/kotlin/net/blockventuremc/modules/backend/Server.kt
@@ -1,12 +1,18 @@
 package net.blockventuremc.modules.backend
 
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.path
+import net.blockventuremc.modules.backend.routes.AudioServer
 
 class Server {
     private lateinit var apiServer: Javalin
 
     fun start() {
-        apiServer = Javalin.create()
+        apiServer = Javalin.create { config ->
+            config.router.apiBuilder {
+                path("/audio", AudioServer())
+            }
+        }
             .get("/") { ctx -> ctx.result("Hello world") }
             .start()
     }
---
+++ src/main/kotlin/net/blockventuremc/modules/backend/routes/AudioServer.kt
@@ -0,0 +1,9 @@
+package net.blockventuremc.modules.backend.routes
+
+import io.javalin.apibuilder.EndpointGroup
+
+class AudioServer: EndpointGroup {
+    override fun addEndpoints() {
+
+    }
+}
