6a781126ba0ff76414179ddd8f7e2778620d6ee8
InvalidJokerDE <0limitdev@gmail.com>
2024-04-28 11:12:24 +0200

    Create BitBoosters.kt

---
+++ src/main/kotlin/net/blockventuremc/modules/boosters/BitBoosters.kt
@@ -0,0 +1,10 @@
+package net.blockventuremc.modules.boosters
+
+import dev.fruxz.ascend.tool.time.calendar.Calendar
+import java.util.UUID
+
+data class BitBoosters(
+    val owner: UUID,
+    val endTime: Calendar,
+    val modifier: Long
+)
