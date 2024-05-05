87755e33dbc13eb55d5e83457ee79dfaeb7f4ce8
CoasterFreakDE <mleaked@gmail.com>
2024-05-05 22:43:51 +0200

    feat: Basic Commands

--- src/main/kotlin/net/blockventuremc/modules/general/model/Ranks.kt
+++ src/main/kotlin/net/blockventuremc/modules/general/model/Ranks.kt
@@ -4,14 +4,10 @@ import net.blockventuremc.modules.general.manager.RankManager
 
 
 enum class Ranks(val rank: Rank) {
-    OWNER(RankManager.getRankByName("owner")),
     ADMIN(RankManager.getRankByName("admin")),
     DEVELOPER(RankManager.getRankByName("developer")),
     MODERATOR(RankManager.getRankByName("moderator")),
-    ENGINEER(RankManager.getRankByName("engineer")),
     BUILDER(RankManager.getRankByName("builder")),
-    CONTENT(RankManager.getRankByName("content")),
-    SUPPORT(RankManager.getRankByName("support")),
     TEAM(RankManager.getRankByName("team")),
     CREATOR(RankManager.getRankByName("creator")),
     CLUBMEMBER(RankManager.getRankByName("clubmember")),
