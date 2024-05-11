3e8b3a1d5a32ef2b0551686d34225c868a4844aa
InvalidJokerDE <0limitdev@gmail.com>
2024-05-11 22:14:24 +0200

    remove complexety

--- src/main/kotlin/net/blockventuremc/utils/RegisterManager.kt
+++ src/main/kotlin/net/blockventuremc/utils/RegisterManager.kt
@@ -18,12 +18,9 @@ import org.bukkit.command.PluginCommand
 import org.bukkit.event.Listener
 import org.bukkit.permissions.Permission
 import org.bukkit.plugin.Plugin
-import kotlin.time.measureTime
 import com.google.common.reflect.ClassPath
-import dev.kord.core.event.Event
 import net.blockventuremc.extensions.getLogger
 import kotlin.reflect.KClass
-import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.primaryConstructor
 
 
@@ -32,116 +29,125 @@ object RegisterManager {
     val dcCommands = mutableListOf<AbstractCommand>()
     private val logger = getLogger()
 
+
+    private fun <T : Any> loadClassesInPackage(packageName: String): List<KClass<T>>? {
+        return loadClassesInPackage(packageName, null)
+    }
+
+    private fun <T : Any> loadClassesInPackage(packageName: String, forEach: (() -> Boolean)?): List<KClass<T>>? {
+        val classLoader = ClassLoader.getSystemClassLoader()
+        return ClassPath.from(classLoader)
+            .allClasses
+            .filter {
+                it.packageName.startsWith(packageName) &&
+                        !it.name.contains('$')
+            }
+            .map {
+                val clazz = it.load().kotlin
+
+                if (forEach?.invoke() == false) {
+                    return null
+                }
+
+                @Suppress("UNCHECKED_CAST")
+                clazz as KClass<T>
+            }
+    }
+
     private suspend fun registerDiscordCommands(kord: Kord, pkg: String) {
+        val commandClasses = loadClassesInPackage<AbstractCommand>(pkg)
 
-        val timeDiscordCommands = measureTime {
-            val reflections = ClassPath.from(Thread.currentThread().contextClassLoader).getTopLevelClasses(pkg)
-            for (clazz in reflections) {
-                val kclass = Class.forName(clazz.name).kotlin
-                if (kclass.isSubclassOf(AbstractCommand::class)) {
-                    val command = kclass.primaryConstructor?.call() as AbstractCommand
+        commandClasses?.forEach {
+            val command = it.primaryConstructor?.call() as AbstractCommand
 
-                    val desc = TranslationCache.get(
-                        Locale.ENGLISH_UNITED_STATES.code,
-                        "discord.commands.${command.name}.description"
-                    )
+            val desc = TranslationCache.get(
+                Locale.ENGLISH_UNITED_STATES.code,
+                "discord.commands.${command.name}.description"
+            )
 
-                    kord.createGlobalChatInputCommand(
-                        command.name,
-                        desc?.message ?: "No description"
-                    ) {
-                        if (command.permission != null) {
-                            defaultMemberPermissions = Permissions(command.permission!!)
-                        }
+            kord.createGlobalChatInputCommand(
+                command.name,
+                desc?.message ?: "No description"
+            ) {
+                if (command.permission != null) {
+                    defaultMemberPermissions = Permissions(command.permission!!)
+                }
 
-                        command.options.invoke(this)
+                command.options.invoke(this)
 
-                        val deDesc =
-                            TranslationCache.get(Locale.GERMAN.code, "discord.commands.${command.name}.description")
+                val deDesc =
+                    TranslationCache.get(Locale.GERMAN.code, "discord.commands.${command.name}.description")
 
-                        if (deDesc != null) {
-                            description(Locale.GERMAN, deDesc.message)
-                        }
-                    }
-
-                    dcCommands.add(command)
-
-                    logger.info("Registered command ${command.name}")
+                if (deDesc != null) {
+                    description(Locale.GERMAN, deDesc.message)
                 }
             }
+
+            dcCommands.add(command)
         }
+
+        logger.info("Registered ${dcCommands.size} discord commands")
+
+
     }
 
     private suspend fun registerDiscordListeners(kord: Kord, pkg: String) {
-        val timeDiscordEvents = measureTime {
-            val reflections = ClassPath.from(Thread.currentThread().contextClassLoader).getTopLevelClasses(pkg)
-            for (clazz in reflections) {
-                val kclass = Class.forName(clazz.name).kotlin
-                if (kclass.isSubclassOf(AbstractEvent::class)) {
-                    val event = kclass.primaryConstructor?.call() as AbstractEvent
+        val eventClasses = loadClassesInPackage<AbstractEvent>(pkg)
 
-                    event.execute(kord)
-
-                    logger.info("Registered event ${event.javaClass.simpleName}")
-                }
-            }
+        eventClasses?.forEach {
+            val event = it.primaryConstructor?.call() as AbstractEvent
+            event.execute(kord)
         }
+
+        logger.info("Registered ${eventClasses?.size} discord events")
     }
 
     private fun registerCommands(pkg: String) {
-        val reflections = ClassPath.from(Thread.currentThread().contextClassLoader).getTopLevelClasses(pkg)
-        var amountCommands = 0
-        for (clazz in reflections) {
-            val kclass = Class.forName(clazz.name).kotlin
-            if (kclass.isSubclassOf(CommandExecutor::class)) {
+        val commandClasses = loadClassesInPackage<CommandExecutor>(pkg)
 
-                val annotation: VentureCommand = kclass.annotations.first { it is VentureCommand } as VentureCommand
+        commandClasses?.forEach {
+            val annotation: VentureCommand = it.annotations.first { it is VentureCommand } as VentureCommand
 
-                val pluginClass: Class<PluginCommand> = PluginCommand::class.java
+            val pluginClass: Class<PluginCommand> = PluginCommand::class.java
 
-                val constructor = pluginClass.getDeclaredConstructor(String::class.java, Plugin::class.java)
+            val constructor = pluginClass.getDeclaredConstructor(String::class.java, Plugin::class.java)
 
-                constructor.isAccessible = true
+            constructor.isAccessible = true
 
-                val command: PluginCommand = constructor.newInstance(annotation.name, VentureLibs.instance)
+            val command: PluginCommand = constructor.newInstance(annotation.name, VentureLibs.instance)
 
 
-                command.aliases = annotation.aliases.toList()
-                command.description = annotation.description
-                command.permission = Permission(annotation.permission, annotation.permissionDefault).name
-                command.usage = annotation.usage
-                val commandInstance = kclass.primaryConstructor?.call() as CommandExecutor
-                command.setExecutor { sender, command, label, args ->
-                    try {
-                        commandInstance.onCommand(sender, command, label, args)
-                    } catch (e: Exception) {
-                        sender.sendMessagePrefixed("An error occurred while executing the command.")
-                        Sentry.captureException(e)
-                        throw e
-                    }
+            command.aliases = annotation.aliases.toList()
+            command.description = annotation.description
+            command.permission = Permission(annotation.permission, annotation.permissionDefault).name
+            command.usage = annotation.usage
+            val commandInstance = it.primaryConstructor?.call() as CommandExecutor
+            command.setExecutor { sender, command, label, args ->
+                try {
+                    commandInstance.onCommand(sender, command, label, args)
+                } catch (e: Exception) {
+                    sender.sendMessagePrefixed("An error occurred while executing the command.")
+                    Sentry.captureException(e)
+                    throw e
                 }
-                command.tabCompleter = commandInstance as? org.bukkit.command.TabCompleter
-
-
-                Bukkit.getCommandMap().register(NAMESPACE_PLUGIN, command)
-                Bukkit.getConsoleSender().sendMessage("Command ${command.name} registered")
-                amountCommands++
             }
+            command.tabCompleter = commandInstance as? org.bukkit.command.TabCompleter
+
+
+            Bukkit.getCommandMap().register(NAMESPACE_PLUGIN, command)
+            Bukkit.getConsoleSender().sendMessage("Command ${command.name} registered")
         }
 
-        logger.info("Registered $amountCommands commands")
+        logger.info("Registered ${commandClasses?.size} commands")
     }
 
     private fun registerListeners(pkg: String) {
-        val reflections = ClassPath.from(Thread.currentThread().contextClassLoader).getTopLevelClasses(pkg)
+        val listenerClasses = loadClassesInPackage<Listener>(pkg)
         var amountListeners = 0
-        for (clazz in reflections) {
-            val kclass = Class.forName(clazz.name).kotlin
-            if (kclass.isSubclassOf(Listener::class)) {
-                val listener = kclass.primaryConstructor?.call() as Listener
-                Bukkit.getPluginManager().registerEvents(listener, VentureLibs.instance)
-                amountListeners++
-            }
+        listenerClasses?.forEach {
+            val listener = it.primaryConstructor?.call() as Listener
+            Bukkit.getPluginManager().registerEvents(listener, VentureLibs.instance)
+            amountListeners++
         }
         logger.info("Registered $amountListeners listeners")
     }
