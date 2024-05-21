066781341a3817bc4ba1f057a099e27f6db2e1c3
InvalidJokerDE <0limitdev@gmail.com>
2024-05-21 11:27:56 +0200

    add notify

--- src/main/kotlin/net/blockventuremc/modules/boosters/BoosterManager.kt
+++ src/main/kotlin/net/blockventuremc/modules/boosters/BoosterManager.kt
@@ -3,11 +3,29 @@ package net.blockventuremc.modules.boosters
 import net.blockventuremc.cache.BoosterCache
 import net.blockventuremc.database.functions.makeBooster
 import net.blockventuremc.database.model.BitBoosters
+import net.blockventuremc.modules.discord.manager.ChannelManager
+import net.blockventuremc.utils.mcasyncBlocking
+import org.bukkit.Bukkit
 
 object BoosterManager {
     fun addBooster(boosters: BitBoosters) {
         makeBooster(boosters)
         BoosterCache.addBooster(boosters)
+
+        val p = Bukkit.getPlayer(boosters.owner) ?: return
+
+        // use unixtimestamp for endTime
+
+        mcasyncBlocking {
+            ChannelManager.sendEconomy {
+                title = "Booster Purchased"
+                description = "A new booster has been purchased by ${p.name}"
+                field {
+                    name = "Modifier"
+                    value = boosters.modifier.toString()
+                }
+            }
+        }
     }
 
     fun getModifiers(uuid: String): Long {
