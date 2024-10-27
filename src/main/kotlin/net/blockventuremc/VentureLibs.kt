90745f2f3cf24236f3419ba3cee46ee6361cef30
InvalidJoker <0limitdev@gmail.com>
2024-10-27 11:09:58 +0100

    WIP: rank in tablist

--- src/main/kotlin/net/blockventuremc/VentureLibs.kt
+++ src/main/kotlin/net/blockventuremc/VentureLibs.kt
@@ -9,7 +9,6 @@ import net.blockventuremc.cache.BoosterCache
 import net.blockventuremc.cache.PlayerCache
 import net.blockventuremc.database.DatabaseManager
 import net.blockventuremc.modules.i18n.TranslationCache
-import net.blockventuremc.modules.placeholders.PlayerPlaceholderManager
 import net.blockventuremc.modules.rides.track.TrackManager
 import net.blockventuremc.modules.structures.StructureManager
 import net.blockventuremc.modules.warps.WarpManager
@@ -72,7 +71,6 @@ class VentureLibs : JavaPlugin() {
         )
 
         logger.info("Registering placeholders...")
-        PlayerPlaceholderManager()
 
         PlayerCache.runOnlineTimeScheduler()
 
